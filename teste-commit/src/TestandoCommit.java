b37a2325335f8ca0f3183ee65d895b3d0db317ee
Brian-smdo <briansouzamdo@gmail.com>
2024-03-08 01:08:53 -0300

    limpando repositorio

--- teste-commit/src/TestandoCommit.java
+++
@@ -1,6 +0,0 @@
-public class TestandoCommit {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Teste de Commit");
-    }
-}
