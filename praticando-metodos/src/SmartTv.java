cbc4211020f507e5f62ee04bdfb6b7b385bf29d9
Brian-smdo <briansouzamdo@gmail.com>
2024-03-07 23:46:04 -0300

    pratica com metodos

---
+++ praticando-metodos/src/SmartTv.java
@@ -0,0 +1,14 @@
+public class SmartTv {
+
+    boolean ligada;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+        
+}
