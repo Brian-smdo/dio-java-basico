07ae03c72fc77f9c6e66498c7fa6f642cb3adaf5
Brian-smdo <briansouzamdo@gmail.com>
2024-03-07 23:36:19 -0300

    teste de commit

---
+++ teste-commit/src/TestandoCommit.java
@@ -0,0 +1,6 @@
+public class TestandoCommit {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Teste de Commit");
+    }
+}
